5452cdbd59efd5911648706518a4be11f27251a1
SomeOne768 <81550632+SomeOne768@users.noreply.github.com>
2022-10-23 00:11:32 +0200

    Game information recovery
    A player can now get every available informations from its own game

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -8,30 +8,43 @@ const router = express.Router()
 router.post("/", (req, res) => {
   // TODO retourner le status 404 si le nom n'existe pas
   if (!req.body.name) return res.status(400).send("Not found");
-  
+
   const newGame = gameService.createGame(req.body.name);
   db.saveGame(newGame)
 
   res.status(201).json({ id: newGame.id, name: newGame.name });
 })
 
-router.get("/", (req, res) =>{
+router.get("/", (req, res) => {
   //TODO retourner la liste des parties existantes
 
-  const gameList = db.getGames().map(elt => ({id: elt.id, name: elt.name}));
+  const gameList = db.getGames().map(elt => ({ id: elt.id, name: elt.name }));
   res.status(200).json(gameList);
 })
 
 
 
 
-router.get("/game/:gameID/players/:playerId", (req, res) =>{
-  //TODO retourner la liste des parties existantes
+router.get("/:gameID/players/:playerId", (req, res) => {
 
-    if(!req.body.gameID || ! req.body.playerId) return res.status(400).send("Uncorrect arguments");
+  if (!req.params.gameID || !req.params.playerId) return res.status(400).send("Uncorrect arguments");
+  if (!(typeof req.body.gameID == typeof int) || !(typeof req.body.playerId == typeof int)) return res.status(404).send("Uncorrect type of arguments");
+  const gameList = db.getGames().filter(game => game.id == req.params.gameID);
 
-    
-    gameService.getGames()
-  
+  let retour = gameList.map(elt => (
+    {
+      currentPlayerIndex: req.params.playerId,
+      name: elt.name,
+      id: elt.id,
+      market: elt.market,
+      tokens: elt.tokens,
+      hand: elt._players[req.params.playerId].hand,
+      camelsCount: elt._players[req.params.playerId].camelsCount,
+      winnerIndex: elt.winnerId,
+      bonusTokens: elt._bonusTokens
+    }))
+  res.status(200).json(retour);
+  // if (gameList === {}) return res.status(404).send("Game not found");
+
 })
 export default router
